a41ab1cac1fa9e82cb391f87a2c81adb14db1e5e
Warren <guileswa@gmail.com>
2020-04-02 16:18:54 -0500

    started assignment 9

---
+++ Assignment 9/Assets/Scripts/EnemyState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface EnemyState
+{
+    void StartPatrol();
+
+    void Move();
+
+    void SpawnUnderlings();
+}
---
+++ Assignment 9/Assets/Scripts/GunScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject Bullet;
+
+    public Transform Muzzle;
+
+    public float fireRate;
+    private float currentTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimer += Time.deltaTime * fireRate;
+
+        if (Input.GetMouseButton(0) && currentTimer > 1)
+        {
+            currentTimer = 0;
+
+            GameObject newBullet = Instantiate(Bullet, Muzzle.position, Quaternion.identity);
+            newBullet.transform.forward = Muzzle.transform.up;
+        }
+    }
+}
